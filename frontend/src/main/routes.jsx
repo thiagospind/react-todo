8fe74f8c5bfd4874325fddf30c08e5361972b66b
Thiago Spindola <thiago.spindola@aapvr.com.br>
2019-08-28 10:36:57 -0300

    desenvolvimento front

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Router, Route, Redirect, hashHistory } from "react-router"
+//import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 
 import Todo from '../todo/todo'
 import About from '../about/about'
--- frontend/src/template/menu.jsx
+++ frontend/src/template/menu.jsx
@@ -4,14 +4,14 @@ export default props => (
     <nav className="navbar navbar-inverse bg-inverse">
         <div className="container">
             <div className="navbar-header">
-                <a className="navbar-brand">
+                <a className="navbar-brand" href="#">
                     <i className="fa fa-calendar-check-o"></i> TodoApp
                 </a>
             </div>
             <div id="navbar" className="navbar-collapse collapse">
                 <ul className="nav navbar-nav">
-                    <li><a href="#/todos">Tarefas</a></li>
-                    <li><a href="#/sobre">Sobre</a></li>
+                    <li><a href='#/todos'>Tarefas</a></li>
+                    <li><a href='#/about'>Sobre</a></li>
                 </ul>
             </div>
         </div>
