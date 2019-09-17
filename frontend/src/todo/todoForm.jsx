8ffda18d63b36c2de31b3864034e452eb9df8d31
Thiago Spindola <thiago.spindola@aapvr.com.br>
2019-09-17 14:15:12 -0300

    front-end

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
 
 export default props => (
     <div role='form' className='todoForm'>
-        <div className='col-xs-12 col-sm-3 col-md-2'>
-            <input id='description' className='form-control' placeholder='Adicione uma tarefa'/>
-        </div>
 
-        <div className='col-xs-12 col-sm-3 col-md-2'>
-            <button className='btn btn-primary'>
-                <i className='fa fa-plus'></i>
-            </button>
-        </div>
+        {/*Componente de layout para telas pequenas, medias e grandes, com os respectivos tamanhos de tela*/}
+        <Grid cols='12 9 10'>
+            <input id='description' className='form-control'
+                   placeholder='Adicione uma tarefa'
+                   onChange={props.handleChange}
+                   value={props.description}/>
+        </Grid>
+
+
+        <Grid cols='12 3 2'>
+            <IconButton style='primary' icon='plus'
+                onClick={props.handleAdd}/>
+            <IconButton style='info' icon='search'
+                onClick={props.handleSearch}/>
+            <IconButton style='default' icon='close'
+                onClick={props.handleClear}/>
+        </Grid>
     </div>
 )
